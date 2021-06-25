184818932c728e4adcc1802c70a0d1e32b5585e0
KIM DONG IL <cappu159@gmail.com>
2021-06-25 11:45:24 +0900

    2-helloworld python ver.

---
+++ 2-helloworld/demo.py
@@ -0,0 +1,16 @@
+# 1. (input)prompt -> "What is your name"
+# 2. SOMEONE func => answer
+# 3. (output) => Hello, SOMEONE, Nice to meet yuou!
+
+
+prompt = input('What is your name?')
+
+def someone(prompt) :
+  print('Hello ' + prompt + ', Nice To meet you!')
+
+someone(prompt)
+
+
+
+
+
